ecc5ae3a1d8a8be6420544a828574bec6626eda5
Nicola Valentini <valentini@omicronconsulting.it>
2021-03-02 17:57:15 +0100

    Created slide book

--- my-virtual-space-ui/src/components/SlideBook.jsx
+++ my-virtual-space-ui/src/components/SlideBook.jsx
@@ -1,7 +1,11 @@
 import React, {useEffect, useState} from 'react';
-
-import 'swiper/swiper-bundle.css';
 import imageApi from "../api/imageApi";
+import {Swiper, SwiperSlide} from 'swiper/react';
+import 'swiper/swiper-bundle.css';
+import SwiperCore, {Autoplay, EffectCoverflow} from "swiper";
+
+SwiperCore.use([EffectCoverflow]);
+SwiperCore.use([Autoplay]);
 
 const SlideBook = () => {
     const [images, setImages] = useState([]);
@@ -15,13 +19,38 @@ const SlideBook = () => {
     }
 
     return (
-        console.log("images", images) || images.length
-            // ? <div className="masthead" style={{
-            //     'background': `url('data:image/jpeg;base64,${images[3]}') no-repeat center center`
-            // }}/>
-            ? <img className='masthead' src={`data:image/jpeg;base64,${images[3]}`} alt='missing_image'/>
-            : <div className="masthead"/>
+        <Swiper
+            slidesPerView={1}
+            loop={true}
+            speed={4000}
+            autoplay={{
+                delay: 3000,
+                disableOnInteraction: true,
+            }}
+            effect='coverflow'
+            coverflowEffect={{
+                rotate: 50,
+                stretch: 0,
+                depth: 100,
+                modifier: 1,
+                slideShadows: true,
+            }}
+        >
+            {!images.length
+                ? <SwiperSlide className="masthead"/>
+                : images.map((image, index) =>
+                    <SwiperSlide key={index}>
+                        <img
+                            className='masthead'
+                            src={`data:image/jpeg;base64,${image}`}
+                            alt='missing_image'
+                        />
+                    </SwiperSlide>)
+            }
+        </Swiper>
     );
 }
+
+// 'background': `url('data:image/jpeg;base64,${images[3]}') no-repeat center center`
 
 export default SlideBook;
