08cf3be3bf52269a3ab7c28d0919c318cedafcd1
Nicola Valentini <valentini@omicronconsulting.it>
2021-07-28 14:54:18 +0200

    Add react-query

--- my-virtual-space-ui/src/App.js
+++ my-virtual-space-ui/src/App.js
@@ -17,6 +17,7 @@ import NotFound from './components/notFound';
 import { handleCheckToken } from './utils/authUtils';
 import { useHistory } from 'react-router';
 import LoadingSpinner from './components/common/loadingSpinner';
+import { QueryClient, QueryClientProvider } from 'react-query';
 
 const Test = ({ num }) => {
   return <h1>TEST {num}</h1>;
@@ -46,38 +47,44 @@ const App = () => {
       });
   }, [history]);
 
+  const queryClient = new QueryClient({
+    defaultOptions: { queries: { refetchOnWindowFocus: false } },
+  });
+
   return (
-    <React.Fragment>
-      <Provider store={store}>
-        <ToastContainer position={toast.POSITION.BOTTOM_RIGHT} />
-        <main>
-          <Header />
-          <SlideBook />
-          <NavBar />
-          {loading ? (
-            <LoadingSpinner />
-          ) : (
-            <Switch>
-              <Route exact path={routes.urls.login} component={Login} />
-              <Route exact path={routes.urls.anime} component={AnimeList} />
-              <Route exact path={routes.urls.test1}>
-                <Test num={1} />
-              </Route>
-              <Route exact path={routes.urls.test2}>
-                <Test num={2} />
-              </Route>
-              <Route exact path={routes.urls.test3}>
-                <Test num={3} />
-              </Route>
-              <Route exact path={routes.urls.home} component={Home} />
-              <Route exact path={routes.urls.notFound} component={NotFound} />
-              <Redirect to={routes.urls.notFound} />
-            </Switch>
-          )}
-        </main>
-        <div className="version-number">{process.env.REACT_APP_VERSION}</div>
-      </Provider>
-    </React.Fragment>
+    <QueryClientProvider client={queryClient}>
+      <React.Fragment>
+        <Provider store={store}>
+          <ToastContainer position={toast.POSITION.BOTTOM_RIGHT} />
+          <main>
+            <Header />
+            <SlideBook />
+            <NavBar />
+            {loading ? (
+              <LoadingSpinner />
+            ) : (
+              <Switch>
+                <Route exact path={routes.urls.login} component={Login} />
+                <Route exact path={routes.urls.anime} component={AnimeList} />
+                <Route exact path={routes.urls.test1}>
+                  <Test num={1} />
+                </Route>
+                <Route exact path={routes.urls.test2}>
+                  <Test num={2} />
+                </Route>
+                <Route exact path={routes.urls.test3}>
+                  <Test num={3} />
+                </Route>
+                <Route exact path={routes.urls.home} component={Home} />
+                <Route exact path={routes.urls.notFound} component={NotFound} />
+                <Redirect to={routes.urls.notFound} />
+              </Switch>
+            )}
+          </main>
+          <div className="version-number">{process.env.REACT_APP_VERSION}</div>
+        </Provider>
+      </React.Fragment>
+    </QueryClientProvider>
   );
 };
 
